82af0697567256114b380b0f2ec6b965f6c29160
antonychristos83 <antonychristos83@gmail.com>
2022-07-09 00:37:51 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrayread1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number : ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+
+                Console.WriteLine("Enter element " + (i + 1) + " : ");
+                arr[i]=Convert.ToInt32(Console.ReadLine());
+            }
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+    }
+}
